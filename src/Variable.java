34b9b1f00da3e23c33006aa385283b44cff99650
SBDaddict <najoon1102@gmail.com>
2024-02-22 22:11:13 +0900

    변수

---
+++ src/Variable.java
@@ -0,0 +1,22 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int yaer = 2000;
+		int age = 14;
+		yaer = yaer + age;
+		
+		System.out.println(yaer);
+		
+		int x = 10;
+		int y = 20;
+		int a = 0;
+		a = x;
+		x = y;
+		y = a;
+		
+		System.out.println("x:"+x+"y:"+y);
+	}
+
+}
